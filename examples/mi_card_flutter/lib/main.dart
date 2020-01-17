a582d62370373671be313672f9ce428c0201c5ab
SuHaLee <gimtommang88@gmail.com>
2020-01-17 12:06:21 +0900

    변수로 색 바꾸는기능 추가

--- examples/mi_card_flutter/lib/main.dart
+++ examples/mi_card_flutter/lib/main.dart
@@ -5,17 +5,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final themeColor = Colors.blue;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: themeColor,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, //세로 주축 기준으로 가운데 정렬
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/me.png'),
+              ),
+              SizedBox(
+                height: 13,
               ),
               Text(
                 'Sue',
@@ -30,41 +35,39 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                     fontFamily: 'SourceSansPro',
                     fontSize: 20,
-                    color: Colors.teal.shade100,
+                    color: themeColor.shade100,
                     letterSpacing: 2.5, //자간
                     fontWeight: FontWeight.bold),
               ),
-              Card(
-                //그림자, 둥근 모서리 등을 만들 수 있음 하지만 padding 불가
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                    horizontal: 25.0,
-                    vertical: 10.0), //상하 , 좌우 의 간격을 동일히 유지(대칭)
-                child: Padding(
-                  //Card class가 padding 을 못 넣어서 padding 클래스로 넣어줌
-                  padding: EdgeInsets.all(25.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        //아이콘과 텍스트 간격을 벌이기 위함
-                        width: 10,
-                      ),
-                      Text(
-                        '010 6416 6680',
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.teal.shade900,
-                            fontFamily: 'SourceSansPro',
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  //나누는 선(horizental line) 추가하는 클래스 1px임
+                  color: themeColor.shade100,
                 ),
               ),
+              Card(
+                  //그림자, 둥근 모서리 등을 만들 수 있음 하지만 padding 불가
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                      horizontal: 25.0,
+                      vertical: 10.0), //상하 , 좌우 의 간격을 동일히 유지(대칭)
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: themeColor,
+                    ),
+                    title: Text(
+                      '010 6416 6680',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: themeColor.shade900,
+                      ),
+                    ),
+                  )),
               Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
@@ -73,14 +76,14 @@ class MyApp extends StatelessWidget {
                   leading: Icon(
                     //텍스트 앞에 오는 아이콘, 자동으로 margin이 생긴다
                     Icons.email,
-                    color: Colors.teal,
+                    color: themeColor,
                   ),
                   title: Text(
                     'gimtommang88@gmail.com',
                     style: TextStyle(
                         fontFamily: 'SourceSansPro',
                         fontSize: 18,
-                        color: Colors.teal.shade900,
+                        color: themeColor.shade900,
                         fontWeight: FontWeight.bold),
                   ),
                 ),
